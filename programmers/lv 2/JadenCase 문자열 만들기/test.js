3c1b01e1254a743e8b8d7ed447868b2a6f45d389
Gil Seongmin(Mark) <ksm65501@gmail.com>
2022-10-22 10:33:50 +0900

    programmers lv2 JadenCase 문자열 만들기
    programmers lv2 JadenCase 문자열 만들기 javascript 문제 풀이

---
+++ programmers/lv 2/JadenCase 문자열 만들기/test.js
@@ -0,0 +1,14 @@
+function solution(s) {
+  var answer = "";
+  for (let i = 0; i < s.length; i++) {
+    if (i === 0 || s[i - 1] === " ") {
+      answer += s[i].toUpperCase();
+    } else {
+      answer += s[i].toLowerCase();
+    }
+  }
+  return answer;
+}
+
+const s = "3people unFollowed me";
+console.log(solution(s));
