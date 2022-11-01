9f85f18e9efe2f1a91429f7ea9963278be991a99
Gil Seongmin(Mark) <ksm65501@gmail.com>
2022-11-01 11:24:18 +0900

    programmers lv2 카펫
    programmers lv2 카펫 javascript 문제 풀이

---
+++ programmers/lv 2/카펫/test.js
@@ -0,0 +1,21 @@
+function solution(brown, yellow) {
+  var answer = [];
+  let row = 0;
+  let col = 2;
+  for (let i = 1; i <= brown / 4; i++) {
+    col++;
+    row = (brown - i * 2) / 2;
+    let mid = i * (row - 2);
+    if (mid === yellow) {
+      answer.push(row, col);
+      break;
+    }
+  }
+
+  return answer;
+}
+
+const brown = 10;
+const yellow = 2;
+
+console.log(solution(brown, yellow));
